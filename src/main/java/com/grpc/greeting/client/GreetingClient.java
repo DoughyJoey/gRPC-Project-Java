4e3515e7c61e80f6b03cc62b4f4502c56b4f9919
DoughyJoey <x15037355@student.ncirl.ie>
2020-03-28 16:33:54 +0000

    first commit

---
+++ src/main/java/com/grpc/greeting/client/GreetingClient.java
@@ -0,0 +1,24 @@
+package com.grpc.greeting.client;
+
+import com.proto.dummy.DummyServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class GreetingClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello i am a gRPC client");
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+
+        System.out.println("Creating stub");
+        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+
+        // do something
+        System.out.println("Shutting down channel");
+        channel.shutdown();
+
+    }
+}
